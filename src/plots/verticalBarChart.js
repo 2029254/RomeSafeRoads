b0272c0fbe02e9f4066a1e0566203536e95f2d01
Mario Angelini <mario@pedius.org>
2023-10-12 18:18:45 +0200

    Bar chart updated

--- src/plots/verticalBarChart.js
+++ src/plots/verticalBarChart.js
@@ -1,35 +1,28 @@
 function drawVerticalBarChart(csvFileName) {
-
-  // set the dimensions and margins of the graph
-  var margin = {top: 20, right: 30, bottom: 40, left:200},
-    width = 610 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-  var svg = d3.select("svg"),
+  let svg = d3.select("svg"),
     margin = 200,
     width = svg.attr("width") - margin,
     height = svg.attr("height") - margin;
 
-
-  var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+  let xScale = d3.scaleBand().range([0, width]).padding(0.4),
     yScale = d3.scaleLinear().range([height, 0]);
 
-  var g = svg.append("g")
+  let g = svg.append("g")
     .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
   d3.csv(csvFileName, function(data) {
     let dataAboutYear = data.filter(function (row) {
       return row['NaturaIncidente'];
     });
-    console.log(dataAboutYear)
+
     let dataAboutYearSorted = dataAboutYear.sort(function (a, b) {
       return d3.ascending(parseFloat(a['NumeroIncidenti']), parseFloat(b['NumeroIncidenti']));
     });
-    console.log(dataAboutYearSorted)
 
-    xScale.domain(data.map(function (d) {
+    xScale.domain(dataAboutYearSorted.map(function (d) {
       return d.NaturaIncidente;
     }));
+
     yScale.domain([0, 12000]);
 
     g.append("g")
@@ -53,7 +46,7 @@ function drawVerticalBarChart(csvFileName) {
       .text("Stock Price");
 
     g.selectAll(".bar")
-      .data(data)
+      .data(dataAboutYearSorted)
       .enter().append("rect")
       .attr("class", "bar")
       .attr("x", function(d) { return xScale(d.NaturaIncidente); })
