7f88ebc932442227ceefba84a2fe9f8ddb2382b6
Mario Angelini <mario@pedius.org>
2023-10-18 11:34:50 +0200

    Choropleth map updated

--- src/plots/choroplethMap.js
+++ src/plots/choroplethMap.js
@@ -1,14 +1,17 @@
-let choroplethMapSvg = d3.select("#choroplethMap")
+const choroplethMapSvg = d3.select("#choroplethMap")
   .append("svg")
-  .attr("width", 1400)
-  .attr("height", 1500);
+  .attr("width", 340)
+  .attr("height", 350)
+  .attr("x", 50)
+  .attr("y", 50);
+
 
 function drawChoroplethMap() {
 // Definisci la proiezione geografica
   let projection = d3.geoMercator()
     .center([12.4964, 41.9028]) // Coordinata centrale per Roma
     .scale(30000)
-    .translate([2300 / 2, 510 / 2]);
+    .translate([340 / 2, 350 / 2]);
 
 // Crea un generatore di percorsi geografici
   let path = d3.geoPath().projection(projection);
@@ -27,8 +30,24 @@ function drawChoroplethMap() {
       .on("click", function (d) {
       //  document.querySelector(".map-container").classList.add("disable-events");
         console.log(d.properties.nome)
-    });
+      })
+      .on("mouseover",  function(d) {
+        d3.select(this).style("fill", "grey");
+        choroplethMapSvg.append("text")
+          .attr("class", "bar-label")
+          .attr("x", 190)
+          .attr("y", 30)
+          .text(d.properties.nome)
+          .style("font-size", "12px");
+      })
+      .on("mouseout", handleMouseOutChoroplethMap);
     //  document.querySelector("#choroplethMap").style.pointerEvents = "auto"
     // document.querySelector("#choroplethMap").style.pointerEvents = "none"
   });
+}
+
+function handleMouseOutChoroplethMap() {
+  d3.select(this)
+    .style("fill", "steelblue");
+  choroplethMapSvg.select(".bar-label").remove();
 }
