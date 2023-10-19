6dc7c2ead713ce17d860458a873ac80e7a9f342e
Mario Angelini <mario@pedius.org>
2023-10-19 13:15:50 +0200

    buttons updated

--- src/plots/weatherButtons.js
+++ src/plots/weatherButtons.js
@@ -1,39 +1,52 @@
 // Dimensioni dei bottoni
 let buttonRadius = 20;
-var buttonPadding = 0; // Spazio tra i bottoni
+let buttonPadding = 0; // Spazio tra i bottoni
 
 const imagePaths = [
   "dataset/img/nuvoloso.png",
   "dataset/img/sole.png",
-  "dataset/img/grandine.png",
   "dataset/img/pioggia.png",
   "dataset/img/neve.png",
   "dataset/img/no-meteo.png"
 ];
 // Funzione per creare bottoni HTML
 function createHTMLButtons() {
-  let buttonData = ["Button 1", "Button 2", "Button 3", "Button 4", "Button 5", "Button 6"];
+  let buttonData = ["Cloudy", "Sunny", "Rainy", "Snowy", "None"];
   const buttonsContainer = document.getElementById("buttons");
 
   buttonData.forEach((buttonText, i) => {
     const button = document.createElement("button");
-    button.id = "Button" + (i + 1); // Assegna un ID univoco
+    const buttonLabel = document.createElement("div"); // Elemento per la scritta
+    button.id = buttonData[i]; // Assegna un ID univoco
     button.className = "circular-button"; // Applica la classe CSS per i bottoni circolari
-    button.style.marginTop = (buttonRadius * 0.9 + buttonPadding) + "px"; // Aggiungi spazio tra i bottoni
+    button.style.marginTop = (buttonRadius * 0.8 + buttonPadding) + "px"; // Aggiungi spazio tra i bottoni
+    button.style.marginLeft = "7px"; // Aggiungi spazio tra i bottoni
     button.style.backgroundSize = "cover"; // Imposta l'immagine per riempire completamente il bottone
     button.style.backgroundImage = `url(${imagePaths[i]})`; // Imposta l'immagine di sfondo del bottone
+    buttonLabel.textContent = button.id; // Imposta il testo del bottone
+    buttonLabel.style.color = "white"; // Aggiungi spazio tra i bottoni
+    buttonLabel.style.fontSize = "9px"; // Aggiungi spazio tra i bottoni
+    buttonLabel.style.marginTop = "25px"; // Aggiungi spazio tra i bottoni
+    //buttonLabel.style.marginLeft = "-1.5px"; // Aggiungi spazio tra i bottoni
+    buttonLabel.style.alignContent = "center"; // Aggiungi spazio tra i bottoni
+
+
+
     button.addEventListener("click", function () {
       console.log("ID del bottone:", this.id);
     });
 
     button.addEventListener("mouseover", function () {
-      console.log("ID del bottone:", this.id);
+      buttonLabel.style.color = "black"; // Mostra il buttonLabel al passaggio del mouse
+      button.style.transform = "scale(1.2)";
     });
 
     button.addEventListener("mouseout", function () {
-      console.log("ID del bottone:", this.id);
+      buttonLabel.style.color = "white"; // Mostra il buttonLabel al passaggio del mouse
+      button.style.transform = "scale(1)";
     });
 
+    button.appendChild(buttonLabel); // Aggiungi il buttonLabel come figlio del bottone
     buttonsContainer.appendChild(button);
   });
 
