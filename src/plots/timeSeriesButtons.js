5792feaef514dde00677c006e50b52c1eb636f61
danielarieti <rieti.1762973@studenti.uniroma1.it>
2023-11-18 19:10:39 +0100

    time series options updated

--- src/plots/timeSeriesButtons.js
+++ src/plots/timeSeriesButtons.js
@@ -9,15 +9,27 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+//Aggiungi un gestore di eventi per ciascun bottone per gestire il clic
+document.getElementById('firstTimesSeriesButton').addEventListener('click', function () {
+    document.getElementById('checkbox1').checked = !document.getElementById('checkbox1').checked;
+});
+document.getElementById('secondTimesSeriesButton').addEventListener('click', function () {
+    document.getElementById('checkbox2').checked = !document.getElementById('checkbox2').checked;
+});
+
+
+
+
+
   buttonsTimeSeries.on('click', function () {
-    buttonsTimeSeries.style('background-color', '#e6e1d5');
+    //buttonsTimeSeries.style('background-color', '#e6e1d5');
     if (buttonsTimeSeries !== this.id) {
       let buttonWeatherValueNew = document.getElementById(buttonWeatherValue);
       buttonWeatherValueNew.style.backgroundColor = "white";
       buttonWeatherValueNew.style.border = "1px solid #d4d0c5";
       buttonWeatherValueNew.style.boxShadow = "0 2px 4px darkslategray";
       buttonWeatherValueNew.style.transform = "scale(1)";
-      buttonWeatherValueNew.style.backgroundImage = `url(${imageClick + "BlackAndWhite/" + buttonWeatherValue + "BW.png"})`;
+      //buttonWeatherValueNew.style.backgroundImage = `url(${imageClick + "BlackAndWhite/" + buttonWeatherValue + "BW.png"})`;
     }
   });
 
